d417cfb93b0c0dbef210cef3536c278d1e223efe
Lovely Tautuaa <lovelytau650@gmail.com>
2024-07-11 20:57:17 -0700

    Created files in models and rendered home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const homeRoutes = new router();
+
+homeRoutes.get("/", async (req, res) => {
+    res.render("homePage");
+})
+
 
 module.exports = homeRoutes;
